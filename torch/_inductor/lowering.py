559d14e33fa02f638a7aa543b76a9622b96e9fbf
Laith Sakka <lsakka@meta.com>
2025-09-28 22:16:11 -0700

    Draft: skip ignore_fresh_unbacked_symbols when applying views in _inplace_generalized_scatter to avoid PendingUnbackedSymbolNotFound error
    ghstack-source-id: c5c6d65876ef46ededc3c96e4c5996e192e45c24
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/164090

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -1836,8 +1836,10 @@ def select(x, dim, idx):
             # Additionally, we want to avoid accidental unbacked unsqueeze semantics. To resolve this,
             # we use as_strided instead.
             # Removing this branch will cause test_unbacked_select_index_with_check to fail.
+            x.realize()
             new_size = x.get_size()
             new_stride = x.get_stride()
+
             new_storage_offset = x.get_layout().offset + new_stride[dim] * actual_index
 
             del new_size[dim]
@@ -1857,7 +1859,7 @@ def select(x, dim, idx):
     assert unbacked_bindings is not None
     assert len(unbacked_bindings) == 1, unbacked_bindings
     unbacked_offset_sym, _ = next(iter(unbacked_bindings.items()))
-
+    x.realize()
     new_size = x.get_size()
     new_stride = x.get_stride()
     new_storage_offset = unbacked_offset_sym
