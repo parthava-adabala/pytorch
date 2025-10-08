f1b72980ed803041c79e261cfa43014ec27a4d18
William Wen <williamwen@meta.com>
2025-10-08 10:50:04 -0700

    [dynamo, nested graph breaks] move cell codegen before side effects codegen
    ghstack-source-id: e561b6de556867979187225789e7527adc687cad
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/160601

--- torch/_dynamo/resume_execution.py
+++ torch/_dynamo/resume_execution.py
@@ -523,7 +523,7 @@ class ContinueExecutionCache:
                             "STORE_FAST", argval=IS_TRACING_RESUME_PROLOGUE_VARNAME
                         ),
                         # finish the call
-                        *create_call_function_ex(False),
+                        *create_call_function_ex(False, False),
                     ]
                 )
             else:
